50a6718a264a45bca252f8226aec46bcaeb35cc6
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-06-02 10:47:13 -0300

    adding pong command and reply on bot

---
+++ src/deploy-commands.ts
@@ -0,0 +1,31 @@
+const { REST, Routes } = require("discord.js")
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const fs = require("node:fs")
+const path = require("node:path")
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter((file: any) => file.endsWith(".ts"))
+
+const commands:any = []
+
+for (const file of commandFiles) {
+   const command = require(`./commands/${file}`)
+   commands.push(command.data.toJSON())
+}
+
+export const deploy_com = async () =>{
+  const rest = new REST({version: "10"}).setToken(process.env.DISCORD_TOKEN);
+
+  try {
+    const data = await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        {body: commands}
+      )
+    }catch (error){
+        console.error(error)
+    }
+}
+
+
